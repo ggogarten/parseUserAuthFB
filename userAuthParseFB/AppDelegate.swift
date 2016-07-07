a6712d1a345759b6e702a2cf4e086e79cd64d62d
George G <ggogarten@gmail.com>
2016-07-07 00:27:15 -0400

    working version

--- userAuthParseFB/AppDelegate.swift
+++ userAuthParseFB/AppDelegate.swift
@@ -21,16 +21,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         Parse.enableLocalDatastore()
         
+        var keys: NSDictionary?
+        
+        if let path = NSBundle.mainBundle().pathForResource("ApiKeys", ofType: "plist") {
+            keys = NSDictionary(contentsOfFile: path)
+        }
+        
+        if let dict = keys {
+            
+            let applicationId = dict["parseApplicationId"] as? String
+            let clientKey = dict["parseClientKey"] as? String
+            let serverURL = dict["parseServer"] as! String
+            
+        
         let parseConfiguration = ParseClientConfiguration(block: { (ParseMutableClientConfiguration) -> Void in
+          
+            
+            ParseMutableClientConfiguration.applicationId = applicationId
+            ParseMutableClientConfiguration.clientKey = clientKey
+            ParseMutableClientConfiguration.server = serverURL
             
         })
         
         Parse.initializeWithConfiguration(parseConfiguration)
         
-        
+        }
         // Override point for customization after application launch.
         return true
     }
+    
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
